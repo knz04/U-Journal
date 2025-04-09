45b27987deaa31c1fd22bf71c40991ba1048d4c8
Opengheimer <intltcsfleader@gmail.com>
2025-04-09 18:18:56 +0700

    feat: onboard page basic functionality

--- app/src/main/java/com/smd/u_journal/MainActivity.kt
+++ app/src/main/java/com/smd/u_journal/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,6 +21,7 @@ import androidx.navigation.compose.rememberNavController
 import com.smd.u_journal.components.BottomNavBar
 import com.smd.u_journal.components.JournalFab
 import com.smd.u_journal.components.TopBar
+import com.smd.u_journal.screens.OnboardingScreen
 import com.smd.u_journal.screens.MainScreen
 import com.smd.u_journal.ui.theme.UJournalTheme
 
@@ -71,7 +73,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(paddingValues),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        MainScreen(navController = navController)
+                        OnboardingScreen()
                     }
                 }
             }
